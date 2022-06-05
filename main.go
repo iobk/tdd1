dc64100efeb5af543653d47ed8613f4d501679b5
kim-yb <ybkimdb@naver.com>
2022-06-06 03:13:26 +0900

    Test() function and calculate funtion create

--- main.go
+++ main.go
@@ -8,3 +8,13 @@ func main() {
 	fmt.Println("Test driven development")
 	Test()
 }
+func Test() {
+	o := calculate("+", 1, 1)
+	if o != 2 {
+		fmt.Printf("Test Failed ! expected %d output : %d \n", 2, o)
+		return
+	}
+}
+func calculate(op string, a, b int) int {
+	return 0
+}

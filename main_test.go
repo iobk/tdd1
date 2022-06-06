ee37d394322bf205e255ffa8262ac7f6ce362c9f
kim-yb <ybkimdb@naver.com>
2022-06-06 12:31:54 +0900

    benchmark test -bench .

--- main_test.go
+++ main_test.go
@@ -13,6 +13,8 @@ func TestTest2(t *testing.T) {
 func TestTest3(t *testing.T) {
 	Test()
 }
+
+/*
 func BenchmarkTest(b *testing.B) {
 	for i := 0; i < b.N; i++ {
 		Test()
@@ -26,5 +28,22 @@ func BenchmarkTest2(b *testing.B) {
 func BenchmarkTest3(b *testing.B) {
 	for i := 0; i < b.N; i++ {
 		Test3()
+	}
+}
+*/
+func BenchmarkCalculate(b *testing.B) {
+	for i := 0; i < b.N; i++ {
+		calculate("*", 5, 5)
+	}
+}
+func BenchmarkCalculate2(b *testing.B) {
+	for i := 0; i < b.N; i++ {
+		Calculate("*", 5, 5)
+	}
+}
+
+func BenchmarkCalculate3(b *testing.B) {
+	for i := 0; i < b.N; i++ {
+		Calculate2("*", 5, 5)
 	}
 }
